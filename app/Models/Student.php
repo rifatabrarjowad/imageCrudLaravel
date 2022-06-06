79b889591b2d8b8d3608c7c7234ae1e37ceda4ae
rifatabrajowad <rajdiprifat2004@gmail.com>
2022-06-07 01:01:18 +0600

    table setup

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     use HasFactory;
+    protected $table = 'students';
+    protected $fillable = [
+        'name',
+        'course',
+        'image'
+    ];
 }
--- database/migrations/2022_06_06_142930_create_students_table.php
+++ database/migrations/2022_06_06_142930_create_students_table.php
@@ -15,6 +15,9 @@ class CreateStudentsTable extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('course');
+            $table->string('image');
             $table->timestamps();
         });
     }
