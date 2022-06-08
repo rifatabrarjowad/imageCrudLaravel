1dc5f339394e9c80dc317b151a6e16d12201ee8f
rifatabrajowad <rajdiprifat2004@gmail.com>
2022-06-08 18:04:20 +0600

    upadate

--- resources/views/student/Home.blade.php
+++ resources/views/student/Home.blade.php
@@ -14,6 +14,33 @@
     @endif
     <h1>Welcome</h1>
     <a href="/AddStudent">Create</a>
+    <table>
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Course</th>
+                <th>Image</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+
+
+
+        <tbody>
+            @foreach($Student as $Student)
+            <tr>
+                <td>{{$Student->name}}</td>
+                <td>{{$Student->course}}</td>
+                <td><img src="{{asset('uploads/student/'.$Student->image)}}" style="width:200px; height: 150px;" alt="">
+                </td>
+
+                <td><a href=" /edit/{{$Student->id}}">Edit</a> | <a href="/delete/{{$Student->id}}">Delete</a></td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+
 </body>
 
 </html>
