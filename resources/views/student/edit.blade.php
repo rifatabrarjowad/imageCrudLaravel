8dac004ef28fc137683a6411f377cf577e419bbe
rifatabrajowad <rajdiprifat2004@gmail.com>
2022-06-24 21:12:02 +0600

    update edit page

--- resources/views/student/edit.blade.php
+++ resources/views/student/edit.blade.php
@@ -16,6 +16,8 @@
         <input type="text" name="name" value="{{$student->name}}" placeholder="Name"> <br><br>
         <input type="text" name="course" value="{{$student->course}}" placeholder="Course"> <br><br>
         <input type="file" name="image" value="{{$student->image}}" placeholder="Image"> <br><br>
+
+        <image width="100" height="100" src="{{asset('uploads/student/'.$student->image)}}" /><br />
         <input type="submit" name="submit" value="Submit"> <br><br>
 
     </form>
